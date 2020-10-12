aa6a10285f6ee349555381d8e18196a59837d7c2
jose sison <jose.sison1@outlook.com>
2020-10-12 15:06:36 -0700

    update

---
+++ RentNowApp/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RentNowApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+namespace RentNowApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
